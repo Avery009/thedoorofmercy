50b5e2ffc7f648b18ca94096a0bb8efda1ce17ec
James Avery King <jamesaveryking@Jamess-MacBook-Pro.local>
2022-07-25 03:38:32 -0400

    Initial Home Page

---
+++ thedoorofmercy/Alms/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AlmsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Alms'
--- thedoorofmercy/Home/views.py
+++ thedoorofmercy/Home/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, Http404
+from django.template import loader
 # Create your views here.
+
+def home(request):
+	template = loader.get_template('home/home.html')
+	#extract authenticated user to pass to home.html
+	#if request.user.is_authenticated:
+		#user = request.user
+	user = 'James'
+	context = {
+		'user' = user
+	}
+	return HttpResponse(template.render(context,request))
+
